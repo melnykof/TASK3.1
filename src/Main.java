0701705e6aad9fc164a7b43c5bb770d61cf048c1
Melnykov Artem <melnykofart@gmail.com>
2020-05-07 15:29:01 +0300

    FIRST COMMIT

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 300;
+        int bonus = 20;
+        int total = price / bonus;
+        System.out.println(total);
+    }
+}
